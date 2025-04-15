490677e32b050216625515edb1f7249ec561f1f2
Seung Lee <seunglee@Seungs-MacBook-Air.local>
2025-04-14 22:31:12 -0400

    merged changes

--- calculator.py
+++ calculator.py
@@ -6,10 +6,7 @@ def subtract(a,b):
 def multiply(a,b):
     return a*b
 
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return b / a
+
 
 def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
