cdd2b26cd9e481474a82447f603fff4f782566d5
Anthony <catdogdaks@gmail.com>
2025-04-14 20:39:22 -0400

    added functions

--- calculator.py
+++ calculator.py
@@ -1,11 +1,20 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
+def add(a,b):
+    return a + b
+def subtract(a,b):
+    return a - b
+def multiply(a,b):
+    return a*b
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
 
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm: base must be > 0 and ≠ 1, and argument must be > 0.")
+    return math.log(b, a)
 
+def exponent(a, b):
+    return a ** b
